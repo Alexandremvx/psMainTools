e7913fb884ce35592b7633f4a8403e3e1a8424bb
Alexandremvx <alexandre.francisco@outlook.com.br>
2016-05-29 01:45:36 -0300

    Disconnect-DomainDrive Function

--- domainFunctions.ps1
+++ domainFunctions.ps1
@@ -9,12 +9,24 @@ Function Connect-DomainDrive {
   [pscredential]$Credential = $(Get-Credential),
   [switch]$Force = $false
  )
- $DomainDrive = Get-PSDrive "$Domain" -ErrorAction SilentlyContinue
+ $DomainDrive = Get-PSDrive $Domain -ErrorAction SilentlyContinue
+ $CreateDomainDrive = $false
  if ($DomainDrive.Provider -eq "ActiveDirectory") {
   Write-Verbose "Domain already connected"
-  if (-not $Force) {$CreateDomainDrive = $false; Write-Verbose "Force option used, reconecting"}
- } elseif ($DomainDrive) {
-  Write-Warning
+  if (-not $Force) {
+   $CreateDomainDrive = $false
+   Write-Verbose "Force option used, reconecting"
+   Disconnect-DomainDrive -Domain $Domain
+  } else {
+   Write-Verbose "Trying reconnect to existent Drive"
+  }
+ } elseif ($DomainDrive -and $DomainDrive.Provider -ne "ActiveDirectory") {
+  Write-Verbose "Drive in use"
+  Write-Warning "Drive in use"
+  $CreateDomainDrive = $false
+  break
+ } else {
+  $CreateDomainDrive = $true
  }
 
  if ($CreateDomainDrive) {
@@ -24,6 +36,22 @@ Function Connect-DomainDrive {
 
 }
 
-Function Disconnect-Domain{}
+Function Disconnect-Domain {
+ Param (
+  [Parameter(Mandatory=$true,Position=1)]
+  [string]$Domain,
+  [switch]$Force = $false
+ )
+ $DomainDrive = Get-PSDrive $Domain
+ if (-not $DomainDrive){
+  Write-Verbose "Drive not in use"
+  break
+ } elseif ($DomainDrive.Provider -ne "ActiveDirectory") {
+  Write-Verbose "Non a DomainDrive"
+  Write-Warning "Non a DomainDrive"
+  break
+ }
+ Remove-PSDrive -Name $Domain -PSProvider ActiveDirectory -Scope Global -Force:$Force
+}
 
 
