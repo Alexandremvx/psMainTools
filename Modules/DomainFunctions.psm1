f891a7147b6307e98f6dc0ed23fd10cf392351db
Alexandremvx <alexandre.francisco@outlook.com.br>
2016-05-29 01:50:16 -0300

    Module Commit

--- Modules/DomainFunctions.psm1
+++ Modules/DomainFunctions.psm1
@@ -1,9 +1,7 @@
-#Requires -Version 3.0 -Modules ActiveDirectory
-#Domain functions (alpha)
-#Experimental
+﻿#Requires -Version 3.0 -Modules ActiveDirectory
+#Domain functions (beta)
 
-
-Function Connect-Domain {
+Function Connect-DomainDrive {
  Param (
   [Parameter(Mandatory=$true,Position=1)]
   [string]$Domain,
@@ -11,17 +9,49 @@ Function Connect-Domain {
   [pscredential]$Credential = $(Get-Credential),
   [switch]$Force = $false
  )
- echo "
- 
- Dominio : $Domain
- Servidor : $Server
- Credencial : $($Credential.username)
- Force : $Force
- "
- 
+ $DomainDrive = Get-PSDrive $Domain -ErrorAction SilentlyContinue
+ $CreateDomainDrive = $false
+ if ($DomainDrive.Provider -eq "ActiveDirectory") {
+  Write-Verbose "Domain already connected"
+  if (-not $Force) {
+   $CreateDomainDrive = $false
+   Write-Verbose "Force option used, reconecting"
+   Disconnect-DomainDrive -Domain $Domain
+  } else {
+   Write-Verbose "Trying reconnect to existent Drive"
+  }
+ } elseif ($DomainDrive -and $DomainDrive.Provider -ne "ActiveDirectory") {
+  Write-Verbose "Drive in use"
+  Write-Warning "Drive in use"
+  $CreateDomainDrive = $false
+  break
+ } else {
+  $CreateDomainDrive = $true
+ }
+
+ if ($CreateDomainDrive) {
+  New-PSDrive -Name $Domain -PSProvider ActiveDirectory -Root "" -Server $Server -Credential $Credential -Scope Global
+ }
+
 
 }
 
-Function Disconnect-Domain{}
+Function Disconnect-Domain {
+ Param (
+  [Parameter(Mandatory=$true,Position=1)]
+  [string]$Domain,
+  [switch]$Force = $false
+ )
+ $DomainDrive = Get-PSDrive $Domain
+ if (-not $DomainDrive){
+  Write-Verbose "Drive not in use"
+  break
+ } elseif ($DomainDrive.Provider -ne "ActiveDirectory") {
+  Write-Verbose "Non a DomainDrive"
+  Write-Warning "Non a DomainDrive"
+  break
+ }
+ Remove-PSDrive -Name $Domain -PSProvider ActiveDirectory -Scope Global -Force:$Force
+}
 
 
